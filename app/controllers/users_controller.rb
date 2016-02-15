7d9b159cd0c9d90b9e5d87ab2f65bd62069ebd9e
aoshima_github <atryorue@gmail.com>
2016-02-15 14:19:55 +0000

    image

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+  
+  def show #追加
+  @user = User.find(params[:id])
+end
+  
   def new
     @user = User.new
   end
@@ -6,6 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      redirect_to @user #修正
     else
       render 'new'
     end
