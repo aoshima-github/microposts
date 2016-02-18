a2c2b3299bada355dd4292c6501654d4dc824512
aoshima_github <atryorue@gmail.com>
2016-02-18 13:32:09 +0000

    rename

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@ class User < ActiveRecord::Base
     has_many :following_users, through: :following_relationships, source: :followed
     
     has_many :follower_relationships, class_name: "Relationship",
-                foregin_key: "followed_id",
+                foreign_key: "followed_id",
                 dependent: :destroy
     has_many :follower_users, through: :follower_relationships, source: :follower
     
